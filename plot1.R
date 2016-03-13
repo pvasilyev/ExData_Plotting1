68ab37d0588f8f07850faf990a0256671568e763
Pavel Vasilev <p_vasiliev@mail.ru>
2016-03-14 02:51:20 +0300

    Update common file, fix minor objections.

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@ draw1 <- function() {
   source("./common.R")
   consumption_data <- get_data()
   
-  hist(filtered_by_date$Global_active_power, 
+  hist(consumption_data$Global_active_power, 
        col = "red", 
        main = "Global Active Power", 
        xlab = "Global Active Power (kilowatts)")
