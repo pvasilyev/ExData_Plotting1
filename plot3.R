189ef6b0f8bc0df1c746f60e9296466446aecd71
Pavel Vasilev <p_vasiliev@mail.ru>
2016-03-14 03:21:55 +0300

    Implement Sub_metering graph over the given date range.

---
+++ plot3.R
@@ -0,0 +1,18 @@
+draw3 <- function() {
+  source("./common.R")
+  consumption_data <- get_data()
+
+  with(consumption_data, plot(Sub_metering_1 ~ Full_Date, 
+                              type = "l", 
+                              xlab = "", 
+                              ylab = "Energy sub metering", 
+                              col = "black"))
+  with(consumption_data, points(Sub_metering_2 ~ Full_Date, type="l", col = "red"))
+  with(consumption_data, points(Sub_metering_3 ~ Full_Date, type="l", col = "blue"))
+  legend("topright", 
+         col = c("black", "red", "blue"), 
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+         lty = 1)
+  dev.copy(png, file = "./plot3.png")
+  dev.off()
+}
